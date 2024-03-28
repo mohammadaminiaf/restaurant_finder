d8cfb35f094b312fa15aef2248184d22a0079c42
Code HQ <codeheadq@gmail.com>
2024-03-28 12:03:51 +0430

    Fixed a small bug and added the video intro.

--- lib/api/get_businesses_by_search_api.dart
+++ lib/api/get_businesses_by_search_api.dart
@@ -4,14 +4,15 @@ import 'package:http/http.dart' as http;
 
 import '/config/config.dart';
 import '/models/business_data.dart';
+import '/services/geolocator.dart';
 
 class GetBusinessesBySearchApi {
   //! You can use this url to curl data in cmd
   // curl -X GET "https://api.yelp.com/v3/businesses/search?latitude=37.786882&longitude=-122.399972" -H "Authorization: Bearer riw09D5Bnk6JfcS_8dOopSIVu0pexxkW7FbOzxVffij9itfgDK4kPx4U2JSMkD1qbNpZGuJb_6aWAhdMm6CnYcjF81jAgXUs5uYFkDUb84TdVmDzZQtgFj_o2KMDZnYx" -H "accept: application/json"
 
   static const baseUrl = 'https://api.yelp.com/v3';
-  static const latitude = 37.786882;
-  static const longitude = -122.399972;
+  // static const latitude = 37.786882;
+  // static const longitude = -122.399972;
 
   static Future<BusinessData> getBusinesses({
     required String text,
@@ -26,10 +27,10 @@ class GetBusinessesBySearchApi {
       'accept': 'application/json',
     };
 
-    // final position = await determinePosition();
+    final position = await determinePosition();
 
-    // final latitude = position.latitude;
-    // final longitude = position.longitude;
+    final latitude = position.latitude;
+    final longitude = position.longitude;
 
     final url = Uri.parse(
       '$baseUrl/businesses/search?term=$text&radius=$radius&categories=$categories&attributes=$attributes&price=$price&open_now=$openNow&limit=50&latitude=$latitude&longitude=$longitude',
